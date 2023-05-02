48b23f1bd334e162311ed575294cd566af83820d
Derek Shih <130016860+DDK038@users.noreply.github.com>
2023-05-01 18:05:42 -0700

    ll

--- script.js
+++ script.js
@@ -20,3 +20,13 @@ function success(position){
 	// Print out the latitude and longitude to see if it works!
     console.log(latitude, longitude);
 }
+
+const btn = document.getElementById("getWeatherBtn");
+btn.addEventListener("click", function(){
+	let forecast = [["M", 52], ["Tu", 53], ["W", 54], ["Th", 55], ["F", 56]]
+    let forecastElements = document.getElementsByClassName("forecast");
+    for (let i = 0; i < forecast.length; i++) {
+        forecastElements[i].innerHTML = forecast[i][0] + ": " + forecast[i][1] + "\u00B0F";
+      }
+
+});
